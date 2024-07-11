c73f4365c40b39834ffaaa7ed269e5a8065ce3e2
Jhayr <jhayrcerna10@gmail.com>
2024-07-11 18:48:05 -0500

    +Proyecto2

--- src/main/java/Conexion/Parametros.java
+++ src/main/java/Conexion/Parametros.java
@@ -8,17 +8,5 @@ public interface Parametros {
     String RUTA="jdbc:mysql://localhost:"+PORT+"/"+DATABASE ;
     String USUARIO="root";
     String CLAVE="";
- 
-    
-    /*
-    *********************************
-    Para setear el nuevo usuario ejecuta estas 3 lineas en mysql,
-    deberia funcinar tmb si usas MyphpAdmin
-    ******************************
-    
-    CREATE USER 'userbdbotica'@'localhost' IDENTIFIED BY 'TodoPor1Sol';
-    GRANT ALTER, CREATE, DELETE, DROP, INDEX, INSERT, REFERENCES, SELECT, UPDATE ON bdbotica.* TO 'userbdbotica'@'localhost';
-    FLUSH PRIVILEGES;
-    
-    */
+
 }
